6697916d2e5ec350aca28da0e1db16f344f89f38
kimflood <kimflood@linecorp.com>
2020-06-17 14:58:58 +0900

    [NO-BTS] init

---
+++ src/main/java/com/sample/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.sample.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
